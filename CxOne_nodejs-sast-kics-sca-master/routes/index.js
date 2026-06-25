28039732d93e079f618514537f6707fedc3bc392
Andrii Radishevskyi <191871656+cx-andrii-radishevskyi@users.noreply.github.com>
2026-06-25 15:19:13 +0300

    Checkmarx Bot - Auto Pull Request

--- CxOne_nodejs-sast-kics-sca-master/routes/index.js
+++ CxOne_nodejs-sast-kics-sca-master/routes/index.js
@@ -8,7 +8,7 @@ var ms = require('ms');
 var streamBuffers = require('stream-buffers');
 var readline = require('readline');
 var moment = require('moment');
-var exec = require('child_process').exec;
+var execFile = require('child_process').execFile;
 var validator = require('validator');
 
 // zip-slip
@@ -158,10 +158,11 @@ exports.create = function (req, res, next) {
     var url = item.match(imgRegex)[1];
     console.log('found img: ' + url);
 
-    exec('identify ' + url, function (err, stdout, stderr) {
-      console.log(err);
+    // Use execFile instead of exec to avoid shell injection: arguments are passed
+    // directly to the process without invoking a shell interpreter.
+    execFile('identify', [url], function (err, stdout, stderr) {
       if (err !== null) {
-        console.log('Error (' + err + '):' + stderr);
+        console.log('execFile error: ' + err.code);
       }
     });
 
